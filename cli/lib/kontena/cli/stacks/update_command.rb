4390772f582725775c1c7f1f0edf1e247c70434c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2016-07-26 11:00:39 +0300

    more specs

--- cli/lib/kontena/cli/stacks/update_command.rb
+++ cli/lib/kontena/cli/stacks/update_command.rb
@@ -7,13 +7,14 @@ module Kontena::Cli::Stacks
     include Common
 
     option ['-f', '--file'], 'FILE', 'Specify an alternate Kontena stack file', attribute_name: :filename, default: 'kontena.yml'
+    option ['-n', '--name'], 'NAME', 'Define stack name (by default comes from stack file)'
 
     def execute
       require_api_url
       token = require_token
       require_config_file(filename)
       stack = stack_from_yaml(filename)
-
+      stack['name'] = name if name
       update_stack(token, stack)
     end
 
